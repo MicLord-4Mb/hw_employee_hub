6078d7f31c55c9c937ab52b6a061e5354b13178a
MicLord <miclord@yahoo.com>
2026-05-16 13:24:13 +0300

    add container to the script

--- src/script.js
+++ src/script.js
@@ -1,4 +1,18 @@
 import {Company, Employee} from "./obj.js";
 
+const myCompany = new Company();
+
+
+const formContainer = document.querySelector('.app-form');
+const inputId = document.getElementById('emp-id');
+const inputName = document.getElementById('emp-name');
+const inputTitle = document.getElementById('emp-title');
+const inputSalary = document.getElementById('emp-salary');
+const btnAdd = document.getElementById('btn-add');
+const btnSearch = document.getElementById('btn-search');
+const btnReset = document.getElementById('btn-reset');
+const inputSearch = document.getElementById('search-query');
+const messageBox = document.getElementById('message-box');
+
 
 
